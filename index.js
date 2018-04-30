15e1695fecaedd65cf50e8a3523b31db6e40c357
Michael Cangcianno <michaelcangcianno@Michaels-MBP.localdomain>
2018-04-30 14:30:25 +0700

    add finished problem solving challenge

---
+++ index.js
@@ -0,0 +1,43 @@
+
+/*
+Pseudocode
+Set member = []
+Set dayOpen = 7 (fitness open date)
+Set tono = 2 (berapa hari sekali tono ke fitness)
+Set anton = 4 (berapa hari sekali anton ke fitness)
+Set budi = 5 (berapa hari sekali budi ke fitness)
+
+Loop through i, i start from dayOpen until 31
+	if (i-7) modular tono equals to 0, then push 'Tono' to member
+	if (i-7) modular anton equals to 0, then push 'Budi' to member
+	if (i-7) modular budi equals to 0, then push 'Anton' to member
+	endif
+	if i modular 5 equals to 0, then print 'tanggal [i]: tempat fitness tutup'
+	else print 'tanggal [i]: [member]''
+	endif
+endfor
+*/
+
+let member = [];
+const dayOpen = 7;
+const tono = 2;
+const anton = 4;
+const budi = 5;
+
+for (var i = dayOpen; i <= 31; i++) {
+	member = [];
+	if ((i - dayOpen) % tono === 0) {
+		member.push('Tono')
+	}
+	if ((i - dayOpen) % anton === 0) {
+		member.push('Anton')
+	}
+	if ((i - dayOpen) % budi === 0) {
+		member.push('Budi')
+	}
+	if (i % 5 === 0) {
+		console.log('Tanggal ' + i + ': Tempat Fitness Tutup')
+	} else {
+		console.log('Tanggal ' + i + ': ' + member.join(', '))
+	}
+}
