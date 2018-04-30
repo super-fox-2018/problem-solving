90ba4258bbf47ac342ad699db23c39b86b8051d0
Tushar <tusharbedi7373@hotmail.com>
2018-04-30 16:10:12 +0700

    created pseudocode and code for probl em

--- index.js
+++ index.js
@@ -4,7 +4,28 @@
   parameters: start(number) -> start Date
               members(array of objects) -> Member details
 Pseudocode:
-  Create
+  Create variable counter = 0
+  FOR i from start to 31
+    create variable output as empty array
+    create variable arrMembers as an empty array
+
+    push (date i :) to output
+
+    IF i%5 is 0 THEN
+      push ('Tempat Fitness Tutup')
+    ELSE
+      FOR j from 0 to members.length
+        IF counter % members[j].name === 0 THEN
+          push (members[j].name) to arrMembers
+        ENDIF
+      ENDFOR
+    ENDIF
+    Create variable result = output + arrMembers.join(', ')
+    log out result
+    counter = counter + 1
+  ENDFOR
+  return ''
+
 
 */
 
@@ -12,20 +33,20 @@ function gymCalender (start, members) {
   let counter = 0;
 
   for (let i = start; i <= 31; i++) {
-    let output = [];
+    let output = []
     output.push('tanggal ' + i + ': ')
-
+    let arrMembers = []
     if (i%5 === 0) {
-      output.push('Tempat Fitness Tutup')
+      arrMembers.push('Tempat Fitness Tutup')
     }
     else {
       for(let j = 0; j < members.length; j++) {
         if(counter % members[j].Often === 0) {
-          output.push(members[j].name + ', ');
+          arrMembers.push(members[j].name);
         }
       }
     }
-    let result = output.join(" ");
+    let result = output + arrMembers.join(', ');
     console.log(result);
     counter += 1;
   } return '';
