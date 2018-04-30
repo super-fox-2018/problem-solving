bd24ec291e3ddd00e355c71adc39d3977e44a767
davidyunus <35801752+davidyunus@users.noreply.github.com>
2018-04-30 19:30:43 +0700

    Update fitnes.js

--- fitnes.js
+++ fitnes.js
@@ -6,7 +6,7 @@ function jadwalFitnes(date, libur, tonoSchedule, antonSchedule, budiSchedule) {
     var output = [];
     
     for(let i = start; i <= date; i++) {
-        if(i == 7) {
+        if(i == start) {
             output.push('Tono');
             output.push('Anton');
             output.push('Budi');
@@ -40,3 +40,43 @@ function jadwalFitnes(date, libur, tonoSchedule, antonSchedule, budiSchedule) {
 }
 
 console.log(jadwalFitnes(31, 5, 2, 4, 5));
+
+/*
+PSEUDOCODE
+
+CREATE function jadwalFitnes and SET parameter to date, tonoSchedule, antonSchedule, budiSchedule
+
+GET start to 7
+GET output to []
+
+LOOP from start until date and add 1 for each loop
+    IF i equals to start
+        ADD output to 'Tono'
+        ADD output to 'Anton'
+        ADD output to 'Budi'
+    ELSE
+        IF i - start % tonoSchedule equals to 0
+            ADD output to 'Tono'
+        END IF
+        IF i - start % antonSchedule equals to 0
+            ADD output to 'Anton'
+        END IF
+        IF i - start % budiSchedule equals to 0
+            ADD output to 'Budi'
+        END IF
+    END IF
+END LOOP
+
+    IF output.length == 1
+        PRINT 'Tanggal ' + i + ': ' + output[0]
+    ELSE IF output.length == 2
+        PRINT 'Tanggal ' + i + ': '+ output[0] + ', ' + output[1]
+    ELSE IF output.length == 3
+        PRINT 'Tanggal ' + i + ': ' + output[0] + ', '+ output[1] + ', ' + output[2]
+    ELSE
+        PRINT 'Tanggal ' + i + ': ' + output
+    END IF
+
+    SET output to []
+
+*/
